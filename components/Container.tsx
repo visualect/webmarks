3f307e6ec3f9d900d71708559426069b3bd1a76f
Yan <visualection@gmail.com>
2023-07-19 18:14:20 +0200

    init commit

---
+++ components/Container.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import React from 'react'
+
+export default function Container({ children }: { children: React.ReactNode }) {
+  return (
+    <div className=''>{children}</div>
+  )
+}
---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import React from 'react'
+import Container from './Container'
+
+export default function Header() {
+  return (
+    <header>
+        <Container>
+            <span>webmarks 📑</span>
+        </Container>
+    </header>
+  )
+}
